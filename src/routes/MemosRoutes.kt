11f29584c3548fb4da7d1804aedf0d29099a1f3f
y.ueki <y.ueki@astamuse.co.jp>
2020-05-09 21:03:41 +0900

    refactor

--- src/routes/MemosRoutes.kt
+++ src/routes/MemosRoutes.kt
@@ -13,7 +13,7 @@ import org.jetbrains.exposed.sql.update
 
 fun Routing.memos() =
     route("memos") {
-        get("") {
+        get {
             val list = transaction {
                 infrastructure.dao.Memo.all().sortedBy { it.id }
                     .map { m -> Memo(m.id.value, m.subject) }
@@ -26,52 +26,50 @@ fun Routing.memos() =
                 HttpStatusCode.Created,
                 mapOf(
                     "memo_id" to transaction {
-                        infrastructure.dao.Memo.new {
-                            this.subject = input.subject
-                        }
+                        infrastructure.dao.Memo.new { this.subject = input.subject }
                     }.id.value
                 )
             )
         }
 
         get("{id}") {
-            val id = call.parameters["id"]?.toInt() ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val id = call.parameters["id"]?.toIntOrNull() ?: return@get call.respond(
+                HttpStatusCode.BadRequest,
+                "Invalid parameter: [${call.parameters["id"]}]"
+            )
             val memoEntity =
                 transaction {
-                    infrastructure.dao.Memo.findById(
-                        id
-                    )
-                }
-                    ?: return@get call.respond(HttpStatusCode.NotFound)
+                    infrastructure.dao.Memo.findById(id)
+                } ?: return@get call.respond(HttpStatusCode.NotFound)
             call.respond(Memo(memoEntity.id.value, memoEntity.subject))
         }
 
         put("{id}") {
-            val id = call.parameters["id"]?.toInt()
-                ?: return@put call.respond(io.ktor.http.HttpStatusCode.Companion.BadRequest)
+            val id = call.parameters["id"]?.toIntOrNull()
+                ?: return@put call.respond(HttpStatusCode.Companion.BadRequest)
             val memo = call.receive<json.MemoContent>()
-            val updatedCount = org.jetbrains.exposed.sql.transactions.transaction {
+            val updatedCount = transaction {
                 infrastructure.dao.Memos.update({ infrastructure.dao.Memos.id eq id }) {
-                    it[infrastructure.dao.Memos.subject] = memo.subject
+                    it[subject] = memo.subject
                 }
             }
             return@put if (updatedCount > 0)
                 call.respond(
-                    io.ktor.http.HttpStatusCode.Companion.OK,
-                    json.Memo(id, memo.subject)
+                    HttpStatusCode.Companion.OK,
+                    Memo(id, memo.subject)
                 )
             else
-                call.respond(io.ktor.http.HttpStatusCode.Companion.NotFound)
+                call.respond(HttpStatusCode.Companion.NotFound)
         }
 
         delete("{id}") {
-            val id = call.parameters["id"]?.toInt()
-                ?: return@delete call.respond(io.ktor.http.HttpStatusCode.Companion.BadRequest)
+            val id = call.parameters["id"]?.toIntOrNull()
+                ?: return@delete call.respond(HttpStatusCode.Companion.BadRequest)
             val deletedCount =
-                org.jetbrains.exposed.sql.transactions.transaction { infrastructure.dao.Memos.deleteWhere { infrastructure.dao.Memos.id eq id } }
+                transaction { infrastructure.dao.Memos.deleteWhere { infrastructure.dao.Memos.id eq id } }
             return@delete if (deletedCount > 0)
-                call.respond(io.ktor.http.HttpStatusCode.Companion.NoContent)
+                call.respond(HttpStatusCode.Companion.NoContent)
             else
-                call.respond(io.ktor.http.HttpStatusCode.Companion.NotFound)
+                call.respond(HttpStatusCode.Companion.NotFound)
         }
     }
